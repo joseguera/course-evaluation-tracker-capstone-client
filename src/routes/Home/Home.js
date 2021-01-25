5e8fff53d3726f9fe0140dcfa0feb6658514b601
Joseguera <joseguera@uclaextension.edu>
2021-01-25 01:11:30 -0800

    added finishing css touches

--- src/routes/Home/Home.js
+++ src/routes/Home/Home.js
@@ -9,22 +9,21 @@ class Home extends Component  {
         return (
             <div className='home-body'>
                 <div className='courses-1'>
-                    <h3 className='home-heading'>What is Courses?</h3>
-                    <p className='home-text'>Courses is a place to save vegan recipes and substitutions (or, courses) by collecting all of those links, brands and countless other things that are often difficult to keep track of.</p>
+                    <h3 className='home-heading'>What is <span className='home-logo'><span className='first-C'>C</span>ourse<span className='last-C'>G</span>rader</span>?</h3>
+                    <p className='home-text'>CourseGrader utilizes the <a href='https://onlinelearningconsortium.org/about/' target='_blank' rel='noreferrer'>Online Learning Consortium</a> (OLC)
+                                            Scorecard as its basis for grading online courses with the purpose of improving the instructor's expertise and
+                                            the students' educational experience.
+                    </p>
                     <p  className='home-text'>
                         <Link className='reg-links' to='/login'>Log in</Link> or <Link className='reg-links' to='/register'>Register</Link> to get started.</p>
                     <p>To view a demo:<br/>
-                        username: samples-account<br/>
-                        password: Password123!
+                        username: <span className='login-creds'>sample</span><br/>
+                        password: <span className='login-creds'>Password123!</span>
                     </p>
                 </div>
-                <div className='courses-2'>
-                    <h3 className='home-heading'>No Courses? No worries</h3>
-                    <p  className='home-text'>Search the web and save the ingredients and recipes that you find here.</p>
-                </div>
                 <div className='courses-3'>
-                    <h3 className='home-heading'>A Courses for all</h3>
-                    <p  className='home-text'>Though Courses was designed with vegans in mind, this app can help anyone save courses and recipes for any dietary choice or lifestyle.</p>
+                    <h3 className='home-heading'>Better courses for all</h3>
+                    <p  className='home-text'>Let's help make online courses that promote learning and engage students.</p>
                 </div>
             </div> 
         )  
